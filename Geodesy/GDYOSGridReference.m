ebc1c7d4e8496ee46fba0f73a414afea6d895fff
Daniel Tull <dt@danieltull.co.uk>
2014-11-19 14:06:34 +0000

    Fix formatting warning

--- Geodesy/GDYOSGridReference.m
+++ Geodesy/GDYOSGridReference.m
@@ -120,8 +120,8 @@
 	
 	NSInteger easting = round((self.easting % 100000) / pow(10, 5 - numberOfFigures));
 	NSInteger northing = round((self.northing % 100000) / pow(10, 5 - numberOfFigures));
-	
-	NSString *format = [NSString stringWithFormat:@"%%@ %%0%id %%0%id", numberOfFigures, numberOfFigures];
+
+	NSString *format = [NSString stringWithFormat:@"%%@ %%0%lid %%0%lid", (long)numberOfFigures, (long)numberOfFigures];
 	return [NSString stringWithFormat:format, self.gridSquareLetters, easting, northing];
 }
 
