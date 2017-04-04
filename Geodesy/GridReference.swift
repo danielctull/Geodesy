2ed27610c6b3b46552558b4bf22d6c38d4ec2ad2
Daniel Tull <dt@danieltull.co.uk>
2017-04-04 12:30:20 +0100

    Allow retrieval of string with number of figures

--- Geodesy/GridReference.swift
+++ Geodesy/GridReference.swift
@@ -5,3 +5,25 @@ public protocol GridReference {
 	var northing: Int { get }
 	var accuracy: Int { get }
 }
+
+extension GridReference {
+
+	private func format(value: Int, withNumberOfFigures figures: Int) -> String? {
+
+		// Can only format for less than 6
+		guard figures < 6 else {
+			return nil
+		}
+
+		let rounded = Int(round(Double(value % 100000) / pow(10, 5 - Double(figures))))
+		return "\(rounded)"
+	}
+
+	public func easting(withNumberOfFigures figures: Int) -> String? {
+		return format(value: easting, withNumberOfFigures: figures)
+	}
+
+	public func northing(withNumberOfFigures figures: Int) -> String? {
+		return format(value: northing, withNumberOfFigures: figures)
+	}
+}
